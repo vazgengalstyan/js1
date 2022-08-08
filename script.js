2f60668095d09aa753ccb90ef59d5cccace7c27a
Gayane Shakhbekyan <shakhbekyang@gmail.com>
2022-08-08 14:44:33 +0400

    Homework2:

--- script.js
+++ script.js
@@ -1,65 +1,77 @@
+// Task 1 DOM tree
 
-//Task 1 js-ում ստանալ հղումները և ամեն երկրորդի գույնը դարձել կարմիր:
-let links = document.getElementsByClassName("link");
+let data = {
+    "Рыбы": {
+      "форель": {},
+      "лосось": {}
+    },
 
-let changeLinksColor = (arr)=>{
-   for(let i = 0; i <arr.length; i++){
-    if(i % 2 !== 0){
-        arr[i].style.color = 'red'
+    "Деревья": {
+      "Огромные": {
+        "секвойя": {},
+        "дуб": {}
+      },
+      "Цветковые": {
+        "яблоня": {},
+        "магнолия": {}
+      }
     }
-   }
-}
-
-changeLinksColor(links);
+  };
 
 
-// Task 2 հղումներից ստանալ href-ները և լցնել մասիվի մեջ:(մասիվը տպել);
+let createTree = (obj)=>{
+  let ul = document.createElement('ul');
+      body.append(ul);
+  for(let key in obj){
+      let li = document.createElement('li');
+          li.innerHTML = key;
+          ul.style.color = 'red'
+      let child = createTree(obj[key]);
+          child.style.color = 'black'
+      if(child){
+          li.append(child);
+      }
+                  ul.append(li)
+  }
 
-let getHrefs =(arr)=>{
-let arr2 = [];
-for(let i = 0; i < arr.length;i++){
-     arr2.push(arr[i].getAttribute('href'))
-}
-console.log(arr2)
-}
-getHrefs(links)
-console.log('_____________');
-
-// Task 3 հղումներին ավելացնել ոչ ստանդարտ attribute: Օրինակ test attribute, ինչ-որ value-ով:
-
-let addAttribute = (arr)=>{
-for(let i = 0; i < arr.length;i++){
-    arr[i].setAttribute('test', 'hello')
-    console.log(arr[i])
-}
+  return ul
 
 }
-addAttribute(links)
-console.log('_____________');
+createTree(data);
 
-// Task 4 հղումներին ավելացնել ստանդարտ target attribute _blank value-ով:
+// Task 2 
+
+let arr = [{element: 'p', content: 'test text', id: 1}, 
+{element: 'div', class: 'block', id: 2},
+{element: 'input', value: 'test'}]
+let x;
+let addElements = (arr) =>{
+      for(let i = 0; i <arr.length; i++){
+           x = document.createElement(arr[i].element);
+          x.innerHTML = arr[i].content?arr[i].content:null;
+              x.value = arr[i].value?arr[i].value:null;
+              if(arr[i].class){
+                  x.classList.add(arr[i].class)
+              }
+          if(arr[i].id){
+              x.id = arr[i].id
+          }
+          body.append(x);
+          console.log(x)
+      }
+  }
 
 
-let addStandartAttribute = (arr)=>{
-    for(let i = 0; i < arr.length;i++){
-        arr[i].setAttribute('target', '_blank')
-        console.log(arr[i])
-    }
-    
-    }
-    addStandartAttribute(links)
-
-    console.log('_____________')
   
-// Task 5 ջնջել href attribute-ը ամեն երկրորդ հղումից:
-let deleteAttribute = (arr) =>{
-    for(let i = 0; i < arr.length; i++){
-        if(i % 2 !== 0){
-            arr[i].removeAttribute('href')
-        }
-        console.log(arr[i]);
-       }
+addElements(arr)
 
-}
+  let div = document.querySelector('.block');
+  div.style.cssText = `height: 100px;
+  width: 100px;
+  margin:10px;
+  border:1px solid black;
+  `
 
-deleteAttribute(links);
+
+
+
