bcc4fca38ede51139d5ead04dc65a4a74f06435c
Artur-Aleqsanyan <110250831+Artur-Aleqsanyan@users.noreply.github.com>
2022-08-10 19:50:01 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,75 +1,21 @@
-let data = {
-  "Рыбы": {
-    "форель": {},
-    "лосось": {}
-  },
+let mark = document.querySelector('#mark')
+let num = document.querySelector('#number')
+let num2 = document.querySelector('#number2')
+let submit = document.querySelector('#submit')
+let text = document.querySelector('#text')
 
-  "Деревья": {
-    "Огромные": {
-      "секвойя": {},
-      "дуб": {}
-    },
-    "Цветковые": {
-      "яблоня": {},
-      "магнолия": {}
-    }
+
+submit.addEventListener("click", function () {
+  if (mark.value === '+') {
+    text.append(Number(num.value) + Number(num2.value)) 
+  }else if (mark.value === '-') {
+    text.append(Number(num.value) - Number(num2.value)) 
+  }else if (mark.value === '/') {
+    text.append(Number(num.value) / Number(num2.value)) 
+  }else if (mark.value === '*') {
+    text.append(Number(num.value) * Number(num2.value)) 
+  }else{
+    text.append('error')
   }
-};
+})
 
-
-let objFunc = data => {
-  let ul = document.createElement('ul');
-  for (let key in data) {
-  let li = document.createElement('li');
-    li.innerText = key;
-
-
-    let valueUl = objFunc(data[key]);
-    if (valueUl) {
-      li.append(valueUl);
-      valueUl.style.color = 'red'
-    }
-
-    ul.append(li);
-  }
-
-  return ul;
-}
-
-
-let woodFunc = (wood, data)=> {
-  wood.append(objFunc(data));
-}
-let wood = document.getElementById('wood');
-woodFunc(wood, data);
-
-let arr = [
-  {element: 'p', content: 'test text', id: 1}, 
-  {element: 'div', class: 'block', id: 2}, 
-  {element: 'input', value: 'test'}
-] 
-
-let arrFunc = arr =>{
-  arr.forEach((item)=> {
-     
-     let elem =document.createElement(item.element)
-      for (let  key in item) {
-          if(key === 'element'){
-              continue
-              
-          }
-          else if(key==='content'){
-              elem.innerText = item[key]
-
-          }
-          else {
-              elem.setAttribute(key,item[key])
-             
-          }
-          document.getElementById('element').append(elem)
-      }
-    
-    });
-
-}
-arrFunc(arr)
