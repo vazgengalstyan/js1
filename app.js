3c5e1046b5133adbf51971bce32f2f8cd1d58c77
Artur-Aleqsanyan <110250831+Artur-Aleqsanyan@users.noreply.github.com>
2022-08-17 15:24:10 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,82 +1,74 @@
-// let block = document.querySelector('#block')
+ let users = [
+    {name:'name1', surname: 'surnem1', age:1},
+    {name:'name2', surname: 'surnem2', age:2},
+    {name:'name3', surname: 'surnem3', age:3},
+    {name:'name4', surname: 'surnem4', age:4},
+    {name:'name5', surname: 'surnem5', age:5},
+    {name:'name6', surname: 'surnem6', age:6},
+    {name:'name7', surname: 'surnem7', age:7},
+    {name:'name8', surname: 'surnem8', age:8},
+    {name:'name9', surname: 'surnem9', age:9},
+    {name:'name10', surname: 'surnem10', age:10},
+    {name:'name11', surname: 'surnem11', age:11},
+    {name:'name12', surname: 'surnem12', age:12},
+    {name:'name13', surname: 'surnem13', age:13},
+    {name:'name14', surname: 'surnem14', age:14},
+    {name:'name15', surname: 'surnem1', age:15},
+    {name:'name16', surname: 'surnem2', age:16},
+    {name:'name3', surname: 'surnem3', age:17},
+    {name:'name4', surname: 'surnem4', age:18},
+    {name:'name5', surname: 'surnem5', age:19},
+    {name:'name6', surname: 'surnem6', age:20},
+    {name:'name7', surname: 'surnem7', age:21},
+    {name:'name8', surname: 'surnem8', age:22},
+    {name:'name9', surname: 'surnem9', age:23},
+    {name:'name10', surname: 'surnem10', age:24},
+    {name:'name11', surname: 'surnem11', age:25},
+    {name:'name12', surname: 'surnem12', age:26},
+    {name:'name13', surname: 'surnem13', age:27},
+    {name:'name14', surname: 'surnem14', age:28}
+]
 
 
-// // 1. mknik@ sharjeluc element@ het@ petq e sharjvi.
-
-// document.addEventListener('mousemove',function (event) {
- 
-//   block.style.top = event.clientY + 'px';
-//   block.style.left = event.clientX + 'px';
-
-// })
 
 
+let table = document.querySelector('#table')
+let paginetion = document.querySelector('#paginetion')
+let notesOnPage = 5;
+let items = []
 
-
-// 2. mknik@ elementi vra bereluc petq e element@ haytnvi ekrani patahakan dirqum
-
-
-// let x = Math.round(Math.random()*110)
-// let y= Math.round(Math.random()*100)
-
-// block.addEventListener('mousemove',function () {
-//  let z= Math.floor(Math.random()*2)
-//   if (z === 0) {
-//     block.style.top = `${block.offsetTop - x}px`;
-//     block.style.left = `${block.offsetLeft - y}px`;
-//   }else if(z === 1){
-//     block.style.top = `${block.offsetTop + x}px`;
-//     block.style.left = `${block.offsetLeft + y}px`;
-//   }
-// })
-
-
-
-
-//3. elementi vra sexmeluc petq e element@ stana random guyn
-
-
-// let changeColor = ()=>{
-//   block.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// block.addEventListener('click', changeColor)
-
-// 5.
-
-// document.addEventListener('click',function(event) {
-//   block.style.top = event.clientY + 'px';
-//   block.style.left = event.clientX + 'px';
-// })
-
-// 6. stexcel html-um forma (anun, axganun, email, tariq, ser, hasce) 
-//nshvac dashterov yev submit sexmeluc tpeq formai arjeqner@ console-um obyekti tesqov
-
-let submit = document.querySelector('#submit');
-
-let name = document.getElementById('name');
-let lastnem = document.getElementById('lastnem');
-let male = document.getElementById('male');
-let female = document.getElementById('female');
-let date = document.getElementById('date');
-let address = document.getElementById('address');
-let email = document.getElementById('email');
-
-let func = () => {
-    let user = {};
-    if (male.value === true) {
-        user.male = male.value;
-    } else {
-        user.female = female.value;
-    }
-    user.name = name.value;
-    user.lastnem = lastnem.value;
-    user.date = date.value;
-    user.address = address.value;
-    user.email = email.value;
-    
-    return user
+let countOfItems = Math.ceil(users.length / notesOnPage)
+for (let i = 1; i <=countOfItems ; i++) {
+    let li = document.createElement('li')
+    li.innerText = i
+    paginetion.appendChild(li)
+    items.push(li)
 }
 
-submit.addEventListener('click', function () {
-  console.log(func());
-});
+for(let item of items){
+    item.addEventListener('click', function () {
+        let pageNum = +this.innerText
+
+        let start = (pageNum - 1) * notesOnPage;
+        let end = start + notesOnPage;
+
+        let notes = users.slice(start, end);
+        table.innerHTML = '';
+        for(let note of notes){
+            let tr = document.createElement('tr')
+            table.appendChild(tr)
+
+            createCell(note.name, tr) 
+            createCell(note.surname,tr)
+            createCell(note.age,tr)
+            
+        }
+    })
+
+}
+
+let createCell = (text,tr)=>{
+    let td = document.createElement('td');
+    td.innerText = text
+    tr.appendChild(td)
+}
